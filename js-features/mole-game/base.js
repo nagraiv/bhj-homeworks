ef8e7f19f1a5c131648634b11dcad91db7a3eb32
GraN <117268180+nagraiv@users.noreply.github.com>
2023-07-07 12:46:16 +0700

    moles base.js improved

--- js-features/mole-game/base.js
+++ js-features/mole-game/base.js
@@ -1,22 +1,39 @@
+// (() => {
+//   let playing = true,
+//     activeHole = 1;
+//
+//   const stop = () => playing = true,
+//     getHole = index => document.getElementById(`hole${index}`),
+//     deactivateHole = index =>
+//       getHole( index ).className = 'hole',
+//     activateHole = index =>
+//       getHole( index ).className = 'hole hole_has-mole',
+//     next = () => setTimeout(() => {
+//       if ( !playing ) {
+//         return;
+//       }
+//       deactivateHole( activeHole );
+//       activeHole = Math.floor( 1 + Math.random() * 9 );
+//       activateHole( activeHole );
+//       next();
+//     }, 800 );
+//
+//   next();
+// })();
+
 (() => {
-  let playing = true,
-    activeHole = 1;
+    const randomHole = () => Math.floor( 1 + Math.random() * 9 );
+    let activeHole = 1; //в начале игры бобр всегда сидит в первой норке согласно разметке страницы
 
-  const stop = () => playing = true,
-    getHole = index => document.getElementById(`hole${index}`),
-    deactivateHole = index =>
-      getHole( index ).className = 'hole',
-    activateHole = index =>
-      getHole( index ).className = 'hole hole_has-mole',
-    next = () => setTimeout(() => {
-      if ( !playing ) {
-        return;
-      }
-      deactivateHole( activeHole );
-      activeHole = Math.floor( 1 + Math.random() * 9 );
-      activateHole( activeHole );
-      next();
-    }, 800 );
+    const getHole = index => document.getElementById(`hole${index}`);
+    const activateHole = index => getHole( index ).className = 'hole hole_has-mole';
+    const deactivateHole = index => getHole( index ).className = 'hole';
 
-  next();
+    const play = () => setInterval(() => {
+        deactivateHole( activeHole );
+        activeHole = randomHole();
+        activateHole( activeHole );
+        }, 800 );
+
+    let isPlaying = play();
 })();
