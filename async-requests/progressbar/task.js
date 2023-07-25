29f9436a2dfe3d2365e5e36561bf398772f02cb2
GraN <117268180+nagraiv@users.noreply.github.com>
2023-07-25 23:12:26 +0700

    progressbar fixed

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,31 +4,24 @@
     const form = document.getElementById('form');
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        const formData = new FormData(form);
-        // const fileEl = document.getElementById("file");
-        // const formData = new FormData();
-        // formData.append('file', fileEl.files[0]);
-        // console.log(formData, fileEl);
         const xhr = new XMLHttpRequest();
+        // xhr.onreadystatechange = () => {
+        //     console.log(xhr);
+        // }
         xhr.open('POST', URL, true);
-        // xhr.enctype='multipart/form-data';
-        // xhr.setRequestHeader('Content-Type', 'multipart/form-data');
-        xhr.send(formData);
-        xhr.onprogress = (e) => {
-            console.log(`Получено ${e.loaded} из ${e.total} байт`);
-            progress.value = (e.loaded / e.total).toFixed(2);
+        xhr.upload.onprogress = (event) => {
+            // console.log(`Получено ${event.loaded} из ${event.total} байт`);
+            progress.value = (event.loaded / event.total).toFixed(2);
         };
-        // xhr.upload.onprogress = (event) => {
-        //     console.log('progress:', event.loaded);
-        //     progress.value = (event.loaded / event.total).toFixed(2);
-        // };
-        xhr.onload = () => {
+        xhr.upload.onload = () => {
             setTimeout(() => {
                 alert('Файл успешно загружен.')
             });
         };
-        xhr.onerror = () => {
-            console.log(`Ошибка при выполнении запроса`);
+        xhr.upload.onerror = () => {
+            console.warn('Ошибка загрузки.');
         };
+        const formData = new FormData(form);
+        xhr.send(formData);
     });
 })();
